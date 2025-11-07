5528dad766c9a319892491242e10cc30122dd707
Afnan Islam <AI736@live.mdx.ac.uk>
2025-11-07 09:48:54 +0000

    new changes

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-print('HELLO WORLD')
-print('hello world')
-print('hello')
+name = input('enter your name: ')
+num = int(input('enter your first number: '))
+numto = int(input('enter your second number: '))
+
+mul = num * numto  
+
+print(f"{name} your answer is {num} x {numto} is {mul}")
