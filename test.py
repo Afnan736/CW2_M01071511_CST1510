2d7c90a9c466d265c34fc0cc09b01cc7d196a4ba
Afnan736 <AI736@live.mdx.ac.uk>
2025-11-07 09:54:08 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ num = int(input('enter your first number: '))
 num2 = int(input('enter your second number: '))
 
 x = num * num2
-print(f"{name} your answer is {num} x {num2} is {x}")
+print(f" Hello {name}")
+print(f"your answer is {num} x {num2} is {x}")
