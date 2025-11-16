634aff8afd5afedb797017cd3e8dcda9cc9879e1
Afnan Islam <AI736@live.mdx.ac.uk>
2025-11-16 20:24:52 +0000

    update main

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from application import register_user, log_in
 
 def menu():
-  print('*' * 20)
-  print('*** Welcome to my ststem***')
-  print('choose from the following option:')
-  print('1. Register')
-  print('2. login')
-  print('3. Exit!!')
-  print('*' * 20)
+    print('*' * 20)
+    print('*** Welcome to my system ***')
+    print('Choose from the following options:')
+    print('1. Register')
+    print('2. Login')
+    print('3. Exit!!')
+    print('*' * 20)
 
 def main():
-  while True :
-    menu ()
-    choice = input('> ')
+    while True:
+        menu()
+        choice = input('> ')
 
-    if choice  =='1':
-      register_user()
-      print('User registered!!')
-    elif choice == '2':
-      log_in()
-      print('you are login')
-    elif choice == '3' :
-      print('good bye!!')
-      break 
+        if choice == '1':
+            register_user()
+        elif choice == '2':
+            result = log_in()
+            if result:
+                print('You are logged in!')
+            else:
+                print('Returning to main menu...')
+        elif choice == '3':
+            print('Good bye!!')
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
 
-main()
+    main()
