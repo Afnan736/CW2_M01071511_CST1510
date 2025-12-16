01f311b45b2d788695ac95ef759c9a7aaee21745
Afnan Islam <AI736@live.mdx.ac.uk>
2025-12-16 19:27:06 +0000

    DONE

--- app/db.py
+++ app/db.py
@@ -1,5 +1,7 @@
 import sqlite3
 
 def get_connection():
- conn = sqlite3.connect('DATA/intelligence_platform.db', check_same_thread =False)
- return conn
+    """Get connection to the SQLite database"""
+    # Create database connection
+    conn = sqlite3.connect('DATA/intelligence_platform.db', check_same_thread=False)
+    return conn
